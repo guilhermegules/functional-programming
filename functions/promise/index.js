487ac1bc813373057995195dda1225e175bcbee3
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-09-14 20:42:51 -0300

    more promises examples

--- functions/promise/index.js
+++ functions/promise/index.js
@@ -14,3 +14,18 @@ generateNumbersBetween(10, 100)
   .then((num) => num * 10)
   .then((numX10) => `Generated number is ${numX10}`)
   .then(console.log);
+
+function workOrNot(value, errorChance) {
+  return new Promise((resolve, reject) => {
+    if (Math.random() < errorChance) {
+      reject("Some error occured!");
+    } else {
+      resolve(value);
+    }
+  });
+}
+
+workOrNot("Testing...", 0.5)
+  .then(console.log)
+  .catch((err) => console.log(`Error: ${err}`))
+  .then(() => console.log("End"));
